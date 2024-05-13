0d4fe5d07eaa04e4182e51687920b3b1b0fecd62
L. Theodore Obonye <ltobonye@gmail.com>
2024-05-13 17:27:58 +0200

    feat: add a team making feature

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,15 +4,72 @@ import {FormsModule} from "@angular/forms";
 import {NgForOf} from "@angular/common";
 import {MatToolbar} from "@angular/material/toolbar";
 import {MatIcon} from "@angular/material/icon";
-import {MatIconButton} from "@angular/material/button";
+import {MatFabButton, MatIconButton} from "@angular/material/button";
 import {MatList, MatListItem} from "@angular/material/list";
 import {MatCard, MatCardContent, MatCardHeader} from "@angular/material/card";
 import {MatGridList, MatGridTile} from "@angular/material/grid-list";
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell, MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef,
+  MatRow, MatRowDef,
+  MatTable
+} from "@angular/material/table";
+import {MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+export interface PeriodicElement {
+  name: string;
+  position: number;
+  weight: number;
+  symbol: string;
+}
 
+
+const ELEMENT_DATA: PeriodicElement[] = [
+  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
+  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
+  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
+  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
+  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
+  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
+  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
+  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
+  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
+  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
+];
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, NgForOf, MatToolbar, MatIcon, MatIconButton, MatList, MatListItem, MatCard, MatCardContent, MatCardHeader, MatGridList, MatGridTile],
+  imports: [RouterOutlet,
+    FormsModule,
+    NgForOf,
+    MatToolbar,
+    MatIcon,
+    MatIconButton,
+    MatList,
+    MatListItem,
+    MatCard,
+    MatCardContent,
+    MatCardHeader,
+    MatGridList,
+    MatGridTile,
+    MatColumnDef,
+    MatHeaderCell,
+    MatTable,
+    MatCell,
+    MatHeaderRow,
+    MatRow,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatHeaderRowDef,
+    MatRowDef,
+    MatFormField,
+    MatInput,
+    MatFabButton,
+    MatLabel
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -20,8 +77,12 @@ export class AppComponent implements OnInit {
   title = 'tappd-poc';
 
   name: string = '';
-  names: string[] = ['Sneakysquid', 'Lethobz', 'Rio Degenaro', 'Dalaflat', 'Marlon', 'Slick', 'MokoneGold', 'Lumbu', 'Pablitoo', 'Gixxer', 'DeadlierSirens', 'Nathi', 'Biggie', 'Mtimande', 'JayJunior', 'Alpha predator', 'Reymotor', 'cameron'];
+  names: string[] = [];
   teams: { id: number, members: string[] }[] = [];
+
+  displayedColumns: string[] = ['name'];
+  dataSource = this.names;
+
 ngOnInit() {
   this.makeTeams();
 }
